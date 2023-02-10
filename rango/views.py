9c42f4348a24e8fb42e6535b41fa6f8f1974848f
Tianchang Wang <2721812w@student.gla.ac.uk>
2023-02-10 02:48:12 +0000

    4

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    # Refer to the TwD book for more information on how this updated view works.
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    # Spoiler: you don't need to pass a context dictionary here.
+    return render(request, 'rango/about.html')
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -1,11 +1,11 @@
-from django.contrib import admin
 from django.urls import path
-from django.urls import include
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+app_name = 'rango'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('rango/', include('rango.urls')),
-# The above maps any URLs starting with rango/ to be handled by rango.
-    path('admin/', admin.site.urls),
+    path('about/', views.about, name='about'),
 ]
